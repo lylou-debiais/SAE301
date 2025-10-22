aa3d0f8d0fa2d1638a4b1c3974ce2bd5be77a23a
lylou-debiais <lylou.debiais@etu.unilim.fr>
2025-10-22 14:55:57 +0200

    vite.conf changer

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/<Nom du dépôt>",
 });
