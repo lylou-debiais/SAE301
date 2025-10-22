e57df59f0eea54e9726e2dc225f70f74687e5462
lylou-debiais <lylou.debiais@etu.unilim.fr>
2025-10-22 16:28:18 +0200

    vite.conf modifier

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+
 export default defineConfig({
-  plugins: [tailwindcss()],
-  base: "/<Nom du dépôt>",
+ plugins: [tailwindcss()],
+ base: "SAE301",
+ build: {
+   rollupOptions: {
+     input: {
+       main: resolve(__dirname, "index.html"),
+       pagecommande: resolve(__dirname, "pagecommande.html"),
+       pageconnexioncompte: resolve(__dirname, "pageconnexioncompte.html"),
+       pagecreationcompte: resolve(__dirname, "pagecreationcompte.html"),
+       pagepanier: resolve(__dirname, "pagepanier.html"),
+       pageproduit: resolve(__dirname, "pageproduit.html"),
+       pagesoinscapillaires: resolve(__dirname, "pagesoinscapillaires.html"),
+       pagesoinscorps: resolve(__dirname, "pagesoinscorps.html"),
+       pagesoinspeau: resolve(__dirname, "pagesoinspeau.html"),
+     },
+   },
+ },
 });
